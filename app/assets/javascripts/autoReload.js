f204c9d9bc7c9d35bf00e0af72a085c04081a514
Mr-Git37 <gits0624@gmail.com>
2020-07-14 18:15:45 +0900

    Update autoReload.js

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -2,40 +2,42 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="MessageBox" data-message-id=${message.id}>
-          <div class="MessageInfo">
-            <div class="MessageInfo__userName">
-              ${message.user_name}
-            </div>
-            <div class="MessageInfo__date">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="Message">
+      `<div class="MessageBox" data-message-id=${message.id}>
+        <div class="list">
+          <div class="list__speaker">
+            ${message.user_name}
+        </div>
+          <div class="list__time">
+            ${message.created_at}
+        </div>
+      </div>
+          <div class="chat-main__message-list__textarea">
             <p class="Message__content">
               ${message.content}
             </p>
-            <img class="Message__image" src="${message.image}">
+              <img class="Message__image" src="${message.image}">
           </div>
-        </div>`
+      </div>`
+        
       return html;
+
     } else {
       let html =
       `<div class="MessageBox" data-message-id=${message.id}>
-        <div class="MessageInfo">
-          <div class="MessageInfo__userName">
+          <div class="list">
+            <div class="list__speaker">
             ${message.user_name}
           </div>
-          <div class="MessageInfo__date">
-            ${message.created_at}
+            <div class="list__time">
+              ${message.created_at}
           </div>
-        </div>
-        <div class="Message">
-          <p class="Message__content">
-            ${message.content}
-          </p>
-        </div>
-      </div>`
+          </div>
+              <div class="chat-main__message-list__textarea">
+                <p class="Message__content">
+                  ${message.content}
+                </p>
+              </div>
+          </div>`
       return html;
     };
   }
@@ -43,7 +45,7 @@ $(function(){
   let reloadMessages = function() {
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     let last_message_id = $('.MessageBox:last').data("message-id");
-    // console.log(last_message_id)
+    console.log(last_message_id)
     $.ajax({
       //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
       url: "api/messages",
